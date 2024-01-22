67dd323519306abd6f4d8978a2ce7eb873277207
DuarteRaphael <raphael-duarte@outlook.com>
2024-01-22 04:42:42 -0300

    primeiro
    primeiro

---
+++ Projeto01.Domain.Core/Interfaces/IEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto01.Domain.Core.Interfaces
+{
+    /// <summary>
+    /// Modelo de interface para entidade de domínio.
+    /// </summary>
+    public interface IEntity
+    {
+        public Guid Id { get; set; }
+        public DateTime CreateAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
---
+++ Projeto01.Services.Api/Controllers/ContatosController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Projeto01.Services.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContatosController : ControllerBase
+    {
+        //Criação
+        [HttpPost]
+        public IActionResult Post()
+        {
+            return Ok();
+        }
+
+        //Modificação
+        [HttpPut]
+        public IActionResult Put()
+        {
+            return Ok();
+        }
+
+        //Exclusão
+        [HttpDelete("{id}")]
+        public IActionResult Delete(Guid id)
+        {
+            return Ok();
+        }
+
+        //Buscar todos
+        [HttpGet("{page}/{limit}")]
+        public IActionResult GetAll(int page, int limit)
+        {
+            return Ok();
+        }
+
+        //Buscas 1 id
+        [HttpGet("{id}")]
+        public IActionResult GetById(Guid id)
+        {
+            return Ok();
+        }
+    }
+}
+
